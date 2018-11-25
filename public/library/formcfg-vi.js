c77fc09b336b935d71a02a22f2089d3c08050cca
styputpro <styput1995@gmail.com>
2018-11-25 15:41:44 +0700

    Updated

---
+++ public/library/formcfg-vi.js
@@ -0,0 +1,126 @@
+webshims.validityMessages.vi = {
+	"typeMismatch": {
+		"defaultMessage": "Vui lòng nhập vào giá trị hợp lệ.",
+		"email": "Vui lòng nhập vào một email.",
+		"url": "Vui lòng nhập vào một URL."
+	},
+	"badInput": {
+		"defaultMessage": "Vui lòng nhập vào giá trị hợp lệ.",
+		"number": "Vui lòng nhập vào số.",
+		"date": "Vui lòng nhập vào ngày.",
+		"time": "Vui lòng nhập vào thời gian.",
+		"range": "Giá trị không hợp lệ.",
+		"month": "Giá trị không hợp lệ.",
+		"datetime-local": "Vui lòng nhập vào giá trị ngày."
+	},
+	"rangeUnderflow": {
+		"defaultMessage": "Giá trị phải lớn hơn hoặc bằng {%min}.",
+		"date": "Giá trị phải bằng hoặc sau {%min}.",
+		"time": "Giá trị phải bằng hoặc sau {%min}.",
+		"datetime-local": "Giá trị phải là hoặc sau {%min}.",
+		"month": "Giá trị phải là hoặc sau {%min}."
+	},
+	"rangeOverflow": {
+		"defaultMessage": "Giá trị phải là không phải là hoặc bằng bằng {%max}.",
+		"date": "Giá trị phải là hoặc trước {%max}.",
+		"time": "Giá trị phải là hoặc trước {%max}.",
+		"datetime-local": "Giá trị phải là hoặc trước {%max}.",
+		"month": "Giá trị phải là hoặc trước {%max}."
+	},
+	"stepMismatch": "Giá trị không hợp lệ.",
+	"tooLong": "Vui lòng nhập vào nhiểu nhất {%maxlength} ký tự.  Bạn đã nhập vào {%valueLen}.",
+	"tooShort": "Vui lòng nhập vào ít nhất {%minlength} ký tự. Bạn đã nhập vào {%valueLen}.",
+	"patternMismatch": "Giá trị không hợp lệ. {%title}",
+	"valueMissing": {
+		"defaultMessage": "Hãy nhập vào trường này.",
+		"checkbox": "Hãy kiểm tra lại hộp này nếu bạn muốn tiếp tục.",
+		"select": "Hãy chọn một tùy chọn.",
+		"radio": "Hãy chọn một tùy chọn."
+	}
+};
+webshims.formcfg.vi = {
+	"numberFormat": {
+		".": ".",
+		",": ","
+	},
+	"numberSigns": ".",
+	"dateSigns": "-",
+	"timeSigns": ":. ",
+	"dFormat": "-",
+	"patterns": {
+		"d": "dd-mm-yy"
+	},
+	"month": {
+		"currentText": "Tháng này"
+	},
+	"time": {
+		"currentText": "Bây giờ"
+	},
+	"date": {
+		"closeText": "Hoàn thành",
+		"clear": "Xóa",
+		"prevText": "Trước",
+		"nextText": "Tiếp",
+		"currentText": "Hôm nay",
+		"monthNames": [
+			"Tháng 1",
+			"Tháng 2",
+			"Tháng 3",
+			"Tháng 4",
+			"Tháng 5",
+			"Tháng 6",
+			"Tháng 7",
+			"Tháng 8",
+			"Tháng 9",
+			"Tháng 10",
+			"Tháng 11",
+			"Tháng 12"
+		],
+		"monthNamesShort": [
+			"Thg 1",
+			"Thg 2",
+			"Thg 3",
+			"Thg 4",
+			"Thg 5",
+			"Thg 6",
+			"Thg 7",
+			"Thg 8",
+			"Thg 9",
+			"Thg 10",
+			"Thg 11",
+			"Thg 12"
+		],
+		"dayNames": [
+			"CN",
+			"Th 2",
+			"Th 3",
+			"Th 4",
+			"Th 5",
+			"Th 6",
+			"Th 7"
+		],
+		"dayNamesShort": [
+			"CN",
+			"Th 2",
+			"Th 3",
+			"Th 4",
+			"Th 5",
+			"Th 6",
+			"Th 7"
+		],
+		"dayNamesMin": [
+			"CN",
+			"Th 2",
+			"Th 3",
+			"Th 4",
+			"Th 5",
+			"Th 6",
+			"Th 7"
+		],
+		"weekHeader": "Tuần",
+		"firstDay": 1,
+		"isRTL": false,
+		"showMonthAfterYear": false,
+		"yearSuffix": ""
+	}
+};
